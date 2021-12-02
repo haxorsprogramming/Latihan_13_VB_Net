f0a917ec3f3baf6f8eeb7877b68eba0b2300e09f
Haxorsprogramming <haxors@uinsu.ac.id>
2021-12-02 16:49:46 +0700

    first commit

---
+++ Latihan_13/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtJudul = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(376, 233)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtJudul
+        '
+        Me.txtJudul.AutoSize = True
+        Me.txtJudul.Location = New System.Drawing.Point(328, 149)
+        Me.txtJudul.Name = "txtJudul"
+        Me.txtJudul.Size = New System.Drawing.Size(75, 15)
+        Me.txtJudul.TabIndex = 1
+        Me.txtJudul.Text = "NadhaMedia"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(881, 528)
+        Me.Controls.Add(Me.txtJudul)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtJudul As Label
+End Class
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Latihan_13_VB_Net" 
