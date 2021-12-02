4f8d676b4192d65dea3f04922d1de875028635a2
Haxorsprogramming <haxors@uinsu.ac.id>
2021-12-02 17:00:08 +0700

    'update'

--- Latihan_13/Form1.Designer.vb
+++ Latihan_13/Form1.Designer.vb
@@ -24,11 +24,13 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.txtJudul = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(376, 233)
+        Me.Button1.Location = New System.Drawing.Point(250, 218)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -44,15 +46,26 @@ Partial Class Form1
         Me.txtJudul.TabIndex = 1
         Me.txtJudul.Text = "NadhaMedia"
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(501, 301)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(881, 528)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.txtJudul)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -60,4 +73,5 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents txtJudul As Label
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Latihan_13/Form1.vb
+++ Latihan_13/Form1.vb
@@ -1,5 +1,16 @@
-﻿Public Class Form1
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Public Conn As OleDbConnection
+    Public DR As OleDbDataReader
+    Public Cmd As OleDbCommand
+    Dim NmKry As String
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Halooo")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
